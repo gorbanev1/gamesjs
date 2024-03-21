05705f34df9b2286a872d187bf80f539c8ef24a8
Gorban <gorbanev_i@mail.ru>
2024-03-21 17:05:11 +0300

    20

--- script.js
+++ script.js
@@ -29,13 +29,22 @@ const CANVAS_HEIGHT = canvas.height = 600
 
 const playerImage = new Image()
 playerImage.src = "shadow_dog.png"
-let x = 0;
+const spriteWidth = 575
+const spriteHeight = 523
+let frameX = 2;
+let frameY = 0
+let gameFrame = 0
+const staggerFrames = 10
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
     // ctx.fillRect(100, 50, 100, 100)
     // ctx.drawImage(image, sx.sy, sw, sh, dx, dy, dw, dh)
-    ctx.drawImage(playerImage, 50, 50, CANVAS_WIDTH, CANVAS_HEIGHT)
+    ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
+    if (gameFrame % staggerFrames == 0)
+        if (frameX < 6) frameX++
+        else frameX = 0
+    gameFrame++
     requestAnimationFrame(animate)
 }
 animate()
