2cc3ba8cf4b735bcd4f75e0b9811323ecc73136f
Gorban <gorbanev_i@mail.ru>
2024-03-25 21:00:01 +0300

    sdsd

--- script.js
+++ script.js
@@ -35,11 +35,35 @@ let frameX = 2;
 let frameY = 0
 let gameFrame = 0
 const staggerFrames = 10
-
+const spriteAnimations = []
+const animationStates = [
+    {
+        name: 'idle',
+        frames: 7,
+    },
+    {
+        name: 'jump',
+        frames: 7,
+    }
+]
+animationStates.forEach((state, index) => {
+    let frames = {
+        loc: [],
+    }
+    for (let j = 0; j < state.frames; j++) {
+        let positionX = spriteWidth * j
+        let positionY = spriteHeight * index
+        frames.loc.push({ x: positionX, y: positionY })
+    }
+    spriteAnimations[state.name] = frames
+})
+console.log(spriteAnimations)
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
     // ctx.fillRect(100, 50, 100, 100)
     // ctx.drawImage(image, sx.sy, sw, sh, dx, dy, dw, dh)
+    let position = Math.floor(gameFrame / staggerFrames) % 6
+    frameX = spriteWidth * position
     ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
     if (gameFrame % staggerFrames == 0)
         if (frameX < 6) frameX++
